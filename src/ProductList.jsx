2d887682e859dad33980db59588bca31c8b40e8a
Roger <roger_carb@hotmail.com>
2024-07-20 05:53:03 -0400

    Add to cart button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -284,7 +284,23 @@ plantsArray.map((element) =>
         </div>
         {!showCart? (
         <div className="product-grid">
-            <ul>{plantList}</ul>
+            {plantsArray.map((category, index) => (
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className="product-list">
+                        {category.plants.map((plant, plantIndex) => (
+                            <div className="product-card" key={plantIndex}>
+                                <img className="product-image" src={plant.image} alt={plant.name} />
+                                <div className="product-title">{plant.name}</div>
+                                <div className="product-description">{plant.description}</div>
+                                <div className="product-cost">{plant.cost}</div>
+                                {/*Similarly like the above plant.name show other details like description and cost*/}
+                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
