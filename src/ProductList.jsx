e66cdc02d0d6285f9e2c3bd40ac3383f408bdba2
Roger <roger_carb@hotmail.com>
2024-07-20 06:03:51 -0400

    Add to cartfunctionality

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,12 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
@@ -247,20 +250,13 @@ const handlePlantsClick = (e) => {
     setShowCart(false);
   };
 
-let plantList = [];
-plantsArray.map((element) => 
-        {
-            plantList.push(<li>{element.category}</li>);
-            element.plants.map((plant) => 
-                {
-                    plantList.push(<li>{plant.name}</li>);
-                    plantList.push(<li><img src={plant.image} width="100" height="100"></img></li>);
-                    plantList.push(<li>{plant.description}</li>);
-                    plantList.push(<li>{plant.cost}</li>);
-                }
-            ) 
-        }
-)
+  const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
+  };
 
     return (
         <div>
